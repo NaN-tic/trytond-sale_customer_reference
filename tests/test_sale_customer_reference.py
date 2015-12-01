831054141e19f661cd07e1a797dc869494ca1708
resteve <resteve@zikzakmedia.com>
2015-12-01 11:18:22 +0100

    Tests

--- tests/test_sale_customer_reference.py
+++ tests/test_sale_customer_reference.py
@@ -1,26 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 # This file is part of the sale_customer_reference module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleCustomerReferenceTestCase(unittest.TestCase):
-    'Test SaleCustomerReference module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_customer_reference')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_customer_reference')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SaleCustomerReferenceTestCase(ModuleTestCase):
+    'Test Sale Customer Reference module'
+    module = 'sale_customer_reference'
 
 
 def suite():
@@ -28,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleCustomerReferenceTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
